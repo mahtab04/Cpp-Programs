fe4b06124761f895ce90730fd339dab4251f4fca
Mahtab Alam <smmalam9@gmail.com>
2020-06-26 21:38:52 +0530

    Folder Name changed

---
+++ Array/weird_algo.cpp
@@ -0,0 +1,24 @@
+// Problem Link-> https://cses.fi/problemset/task/1068/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   long long int n;
+    cin >> n;
+    cout << n << " ";
+    while (n > 1)
+    {
+        if (n % 2 == 0)
+        {
+            n = n / 2;
+        }
+        else
+        {
+            n = 3 * n+1;
+        }
+        cout << n << " ";
+    }
+}
